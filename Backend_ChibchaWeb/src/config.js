0c4b3f1047fa2074d90377af05d646e02835d26b
JhonMales <61199494+JhonMales@users.noreply.github.com>
2023-11-30 20:06:32 -0500

    Update config.js

--- Backend_ChibchaWeb/src/config.js
+++ Backend_ChibchaWeb/src/config.js
@@ -1,2 +1,2 @@
 export const TOKEN_SECRET = 'some secret key';
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const FRONTEND_URL = 'https://frontend-app-mjnx.onrender.com';
