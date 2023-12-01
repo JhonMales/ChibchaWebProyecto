28b8780c470852024da5e90f1d41598ac110605a
JhonMales <61199494+JhonMales@users.noreply.github.com>
2023-11-30 20:55:28 -0500

    Update constantes.js

--- Frontend_ChibchaWeb/src/constantes.js
+++ Frontend_ChibchaWeb/src/constantes.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
-const backend_url = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000/api'
+const backend_url = import.meta.env.VITE_backend_url || 'http://localhost:3000/api'
 
 export { backend_url }
